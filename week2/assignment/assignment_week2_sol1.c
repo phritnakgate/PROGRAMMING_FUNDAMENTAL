e511a6ee17531bcf8b138ab9913ddab1df12b77f
Phrit Nakgate <boss5500216@gmail.com>
2022-09-07 11:28:34 +0700

    Update assignment_week2_sol1.c

--- week2/assignment/assignment_week2_sol1.c
+++ week2/assignment/assignment_week2_sol1.c
@@ -6,7 +6,10 @@ void main(){
 	printf("Height = ");
 	scanf("%f",&h);
 	if(h <= 1){
-		printf("Please input more than 1!");
+		total = h;
+		i = 1;
+		printf("Distance = %.2f \n",total);
+		printf("Bounce count = %d",i);
 	}else{
 		total = h;
 		for(i=0;h > 1;i++){
